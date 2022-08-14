fd8cad2a4d27a29273c6b9d9ae3e5890ac03a0fe
rijal <jeryzal333@gmail.com>
2022-08-14 11:00:36 +0700

    landing page left side finished

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -2,14 +2,31 @@
   <div class="q-pa-md q-container" style="margin-top:96px;">
     <div class="row justify-center">
       <div class="col-md-5">
-        <div class="column" style="height:150px;">
-            <span class="col-10 text-h3 text-weight-bold ">
+        <div class="column" style="height:450px;">
+            <span class="col-4 text-h3 text-weight-bold ">
                 Mudah belanjanya,<br> Mudah bayarnya
             </span>
             <br/>
             <span class="col-2 text-h6 text-weight-regular">
                 Kami menyediakan banyak <br>
                 Metode pebayaran yang mudah.
+            </span>
+            <span class="col-2">
+                <q-btn color="primary" label="Mulai pencarian" />
+            </span>
+            <span class="col-1 row">
+                <div class="col-3">
+                    <q-icon class="text-h3 text-grey-8" :name="fasGift" />
+                    <p class="text-weight-bold">Puluhan produk</p>
+                </div>
+                <div class="col-3">
+                    <q-icon class="text-h3 text-grey-8" :name="fasHandsHelping" />
+                    <p class="text-weight-bold">Transaksi mudah</p>
+                </div>
+                <div class="col-3">
+                    <q-icon class="text-h3 text-grey-8" :name="fasBusinessTime" />
+                    <p class="text-weight-bold">Hemat waktu</p>
+                </div>
             </span>
         </div>
       </div>
@@ -22,8 +39,11 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { fasGift, fasHandsHelping, fasBusinessTime } from '@quasar/extras/fontawesome-v5'
 
 export default defineComponent({
-    
+    setup() {
+        return { fasGift, fasHandsHelping, fasBusinessTime }
+    }
 })
 </script>
